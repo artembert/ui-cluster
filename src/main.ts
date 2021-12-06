9c17111e87e51d1c0c4c559d807fa1b91b82c134
Artem Beresnev <artembert@gmail.com>
2021-12-07 00:12:50 +0300

    Remove random moving circles

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
 import "./style.css";
 import "maplibre-gl/dist/maplibre-gl.css";
 import maplibregl from "maplibre-gl";
-import {
-  movingCirclesInit,
-  movingCirclesSourceConfig,
-} from "./components/moving-circles";
 import { getPoints } from "./data/points";
 import { renderCircleChartMarkers } from "./components/circle-chart";
 
@@ -17,37 +13,17 @@ const map = new maplibregl.Map({
     "https://api.maptiler.com/maps/streets/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL",
 });
 
-movingCirclesInit();
-
 const points = getPoints();
 renderCircleChartMarkers(map, points);
 
 map.on("load", function () {
-  map.addSource(movingCirclesSourceConfig.name, {
-    type: "canvas",
-    canvas: movingCirclesSourceConfig.elementId,
-    coordinates: [
-      [91.4461, 21.5006],
-      [100.3541, 21.5006],
-      [100.3541, 13.9706],
-      [91.4461, 13.9706],
-    ],
-    // Set to true if the canvas source is animated. If the canvas is static, animate should be set to false to improve performance.
-    animate: true,
-  });
-  map.addLayer({
-    id: "canvas-layer",
-    type: "raster",
-    source: movingCirclesSourceConfig.name,
-  });
-
-  map.addSource("points", {
-    type: "geojson",
-    data: points,
-  });
-  map.addLayer({
-    id: "points",
-    type: "circle",
-    source: "points",
-  });
+  // map.addSource("points", {
+  //   type: "geojson",
+  //   data: points,
+  // });
+  // map.addLayer({
+  //   id: "points",
+  //   type: "circle",
+  //   source: "points",
+  // });
 });
