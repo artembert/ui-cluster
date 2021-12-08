344f6557e778e160981871c582eb74f5d634694b
Artem Beresnev <artembert@gmail.com>
2021-12-08 20:18:32 +0300

    Add nested pages: vector-tiles and mock-json-data

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  base: "/ui-cluster",
+  base: "/ui-cluster/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        vectorTiles: resolve(__dirname, "vector-tiles.html"),
+        mockMarkers: resolve(__dirname, "mock-json-data.html"),
+      },
+    },
+  },
 });
