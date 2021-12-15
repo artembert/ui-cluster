1437ff15bafcb5b7c93e10de9b649c3b3725a778
Artem Beresnev <artembert@gmail.com>
2021-12-15 17:00:40 +0300

    Hide original points

--- src/configs/cluster-style.config.ts
+++ src/configs/cluster-style.config.ts
@@ -4,5 +4,5 @@ export const clusterStyleConfig: CirclePaint = {
   "circle-color": "#777777",
   "circle-stroke-color": "transparent",
   "circle-stroke-width": 0,
-  "circle-radius": 5,
+  "circle-radius": 0,
 };
